297e86a36f7355d1591dae74d37ebdddbcf6f260
Alex Choi <alex@medfin.ai>
2025-09-20 19:47:10 -0400

    remove the border

--- frontend/src/app/projects/[id]/page.tsx
+++ frontend/src/app/projects/[id]/page.tsx
@@ -85,17 +85,7 @@ export default function ProjectPage() {
   }
 
   return (
-    <div className="w-full min-h-screen relative bg-[#F7F5F3] overflow-x-hidden flex flex-col justify-start items-center max-w-[100vw]">
-      <div className="relative flex flex-col justify-start items-center w-full max-w-[100vw] overflow-x-hidden">
-        <div className="w-full max-w-none px-4 sm:px-6 md:px-8 lg:px-0 lg:max-w-[1060px] lg:w-[1060px] relative flex flex-col justify-start items-start min-h-screen overflow-x-hidden">
-          {/* Vertical lines */}
-          <div className="w-[1px] h-full absolute left-4 sm:left-6 md:left-8 lg:left-0 top-0 bg-[rgba(55,50,47,0.12)] shadow-[1px_0px_0px_white] z-0" />
-          <div className="w-[1px] h-full absolute right-4 sm:right-6 md:right-8 lg:right-0 top-0 bg-[rgba(55,50,47,0.12)] shadow-[1px_0px_0px_white] z-0" />
-
-          <Navigation />
-
-          {/* Main Content Area with Chat and Canvas */}
-          <div className="w-full flex-1 relative z-10 mt-16">
+          <div className="w-full flex-1 relative z-10">
             <div className="flex h-[calc(100vh-64px)] bg-gray-50">
               {/* Left side - Chat Interface */}
               <div className="w-[480px] border-r border-gray-200 bg-white">
@@ -112,8 +102,5 @@ export default function ProjectPage() {
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
   )
 }
