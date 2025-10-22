159bd118619289773248ae6d3ebf7275baa6e55f
alexchvod <uceiac8@ucl.ac.uk>
2025-10-22 21:51:36 +0100

    add X authentication

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -14,6 +14,7 @@ import {
   GitHubIcon,
   NotionIcon,
   SlackIcon,
+  XIcon,
 } from "../../components/ProviderIcons";
 import { Skeleton } from "../../components/ui/skeleton";
 
@@ -75,6 +76,13 @@ export default function SettingsPage() {
       name: "Slack",
       description: "Integrate with Slack channels",
       icon: SlackIcon,
+      enabled: true,
+    },
+    {
+      id: "x",
+      name: "X",
+      description: "Integrate with X (Twitter)",
+      icon: XIcon,
       enabled: true,
     },
   ];
